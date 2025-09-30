cae61fb3c9dbe0502c9321a675b33ffd1f5276be
khagendraadhikari <kadhikari@my.yearupunited.org>
2025-09-30 10:33:05 -0500

    billing fixed

--- src/main/java/com/plurlasight/Main.java
+++ src/main/java/com/plurlasight/Main.java
@@ -14,27 +14,35 @@ public class Main {
         String fullName = scanner.nextLine();
         output.append(fullName).append("\n\n");
 
+        // billing
+
         System.out.println("Billing street");
         String billingStreet= scanner.nextLine();
         output.append("billing address:\n").append(billingStreet). append("\n");
 
-        System.out.println("Shipping city; ");
-        String shippingCity= scanner.nextLine();
-        output.append(shippingCity).append(", ");
 
-        System.out.println("shipping state: ");
-        String shippingState = scanner.nextLine();
-        output.append(shippingState).append(" ");
+        System.out.println("Billing city; ");
+        String billingCity= scanner.nextLine();
+        output.append(billingCity).append(", ");
 
-        System.out.println("shipping zip: ");
-        String shippingZip = scanner.nextLine();
-        output.append(shippingZip).append("\n");
+        System.out.println("Billing state: ");
+        String billingState = scanner.nextLine();
+        output.append(billingState).append(" ");
+
+        System.out.println("Billing zip: ");
+        String billingZip = scanner.nextLine();
+        output.append(billingZip).append("\n");
+
+        // shipping
+
+
 
         // display of the customer info.
 
         System.out.println("\nDisplay the customer information\n");
 
         System.out.println(output.toString());
+
 
     }
 }
